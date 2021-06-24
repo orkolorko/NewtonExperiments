99677a7d1c2ac848d55242b1fd0ffad6e1b6751e
João Victor <joaovictovelozo@gmail.com>
2021-06-24 14:12:03 -0300

    versão nova

--- src/NewtonBasins.jl
+++ src/NewtonBasins.jl
@@ -1,10 +1,10 @@
-using Images, FileIO, ImageIO, ImageMagick
+using Images, FileIO, ImageIO, ImageMagick, DualNumbers
 
 function NewtonBasin(k = 10; x_min  = -1.0, x_max = 1.0, x_steps = 1024,
                         y_min  = -1.0, y_max = 1.0, y_steps = 1024
                 )
-        f(x) = x^3-1
-        fprime(x) = 3*x^2
+        f(x) = x^50+8*x^5-80/3*x^4+20*x^3-2*x+1
+        fprime(x) = 50*x^49+40*x^4-320/3*x^3+60*x^2-2
 
         A  = [x+im*y for y in reverse(LinRange(y_min, y_max, y_steps)), x in LinRange(x_min, x_max, x_steps)]
         #@info A
@@ -44,3 +44,6 @@ color_list = [  RGB(202/255, 179/255, 159/255),
                 RGB(175/255, 199/255, 208/266),
                 RGB(195/255, 111/255, 49/255),
                 RGB(185/255, 203/255, 153/255)]
+
+cores_joao(i; t=1000) = RGB( mod(i^2+i*π, 255)/255, mod(i^2+i*π, 255)/255, mod(4*i*π, 255)/255)
+color_list2 = [cores_joao(i) for i in 1:1000]
